5ac2231b1ad73a705315d2ff95b299bf8cae8133
Monica2402 <monica2402.mb@gmail.com>
2018-03-27 19:36:17 +0200

    BookAPI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,10 @@ import API.BookAPI;
 public class Main {
     public static void main(String[]args)
     {
-<<<<<<< HEAD
         BookAPI bAPI = new BookAPI();
 
         System.out.println(bAPI.search("Illuminae"));
-=======
 
         System.out.println("Hello world");
->>>>>>> 59a6d4db67c2d1afa1eef3a4de17fd58afc71703
     }
 }
