a5fb4cce104a8ad45dd4055cd68e858578ca034e
Monica2402 <monica2402.mb@gmail.com>
2018-03-27 19:33:32 +0200

    BookAPI

--- src/main/java/API/BookAPI.java
+++ src/main/java/API/BookAPI.java
@@ -1,4 +1,34 @@
 package API;
+import okhttp3.*;
 
 public class BookAPI {
+    private final String URL = "https://www.goodreads.com/search.xml?";
+    private final String key = "key=RIWQy80W6TBDYlMptXJyQ";
+    private final String secret = "iw2K3xHouX1j9mCfQwjGEX79UgQpB7FBOFN0pUWzuU";
+
+    private OkHttpClient client;
+    private Request request;
+    private Response response;
+
+    public BookAPI()
+    {
+        client = new OkHttpClient();
+        request = null;
+        response = null;
+    }
+
+    public String search(String query)
+    {
+        request = new Request.Builder().url(URL+key+"&q="+query).get().build();
+        String finalResponse = "";
+        try{
+            response = client.newCall(request).execute();
+            finalResponse = response.body().string();
+        }
+        catch (Exception e)
+        {
+            System.out.print("The Book API is not available.");
+        }
+        return finalResponse;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import API.BookAPI;
+
 public class Main {
+    public static void main(String[]args)
+    {
+        BookAPI bAPI = new BookAPI();
+
+        System.out.println(bAPI.search("Illuminae"));
+    }
 }
